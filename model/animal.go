43c7dd3c98b424bbc9f414cac0e49fa13b26a437
Shanessa Kostaman <Shanessa.m7493@gmail.com>
2023-06-07 16:46:54 +0200

    forgot to save this one

--- model/animal.go
+++ model/animal.go
@@ -1,20 +1,16 @@
 package model
 
-import "time"
-
 type Animal struct {
-	AnimalID    int64     `json:"animal_id"`
-	Picture     string    `json:"picture"`
-	FirstName   string    `json:"first_name"`
-	LastName    string    `json:"last_name"`
-	DateOfBirth string    `json:"date_of_birth"`
-	Gender      string    `json:"gender"`
-	Type        string    `json:"type"`
-	Breed       string    `json:"breed"`
-	Shelter     string    `json:"shelter"`
-	Address     string    `json:"address"`
-	PostalCode  string    `json:"postal_code"`
-	Bio         string    `json:"bio"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	AnimalID    int64  `json:"animal_id"`
+	Picture     string `json:"picture"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	DateOfBirth string `json:"date_of_birth"`
+	Gender      string `json:"gender"`
+	Type        string `json:"type"`
+	Breed       string `json:"breed"`
+	Shelter     string `json:"shelter"`
+	Address     string `json:"address"`
+	PostalCode  string `json:"postal_code"`
+	Bio         string `json:"bio"`
 }
